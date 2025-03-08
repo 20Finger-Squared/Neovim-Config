8d7194867d3e68266f9b831a9c8086d3f3b7d4d5
Rhylie <orhylie@gmail.com>
2025-03-08 10:48:34 +0000

    removed test file

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,9 +5,10 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.styelua,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.prettier,
       }
     })
   end

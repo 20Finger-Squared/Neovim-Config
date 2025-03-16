980711729d200f9aa308316c981da09ede875c02
Rhylie <orhylie@gmail.com>
2025-03-16 00:16:51 +0000

    removed redundancy and fixed spellcheck

--- lua/plugin-options.lua
+++ lua/plugin-options.lua
@@ -1,3 +1,2 @@
 -- used for options that need to be loaded after plugins not strictly just for plugin options
 vim.cmd("colorscheme gruvbox-material")
-vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("setlocal spell spelllang=en_uk")
+vim.cmd("setlocal spell spelllang=en_gb")
 vim.g.mapleader = " "
 vim.cmd("set relativenumber")
 vim.cmd("set termguicolors")

234e4cca25c87910e244c3d6a58dec8c6adfd534
Rhylie <orhylie@gmail.com>
2025-03-22 20:44:58 +0000

    fixed mason keymaps
    Fixed the keymaps due to updating keymaps

--- lua/mappings/mason-lspconfig.lua
+++ lua/mappings/mason-lspconfig.lua
@@ -1,6 +1,5 @@
 local set = vim.keymap.set
-set("n", "<leader>fm", vim.lsp.buf.format(),     { desc = "Format code" })
-set("n", "<leader>ca", vim.lsp.buf.code_action() { desc = "Show code actions" })
-set("n", "gd",         vim.lsp.buf.definition(), { desc = "Go to definition" })
-set("n", "K",          vim.lsp.buf.hover(),      { desc = "Show hover documentation" })
-
+set("n", "<leader>fm", function() vim.lsp.buf.format() end,      { desc = "Format code"              })
+set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "Show code actions"        })
+set("n", "gd",         function() vim.lsp.buf.definition() end,  { desc = "Go to definition"         })
+set("n", "K",          function() vim.lsp.buf.hover() end,       { desc = "Show hover documentation" })
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,43 +1,65 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("bufferline").setup({
-      options = {
-        themable = true,
-        diagnostics = "nvim_lsp",
-        show_close_icon = false,
-        separator_style = "padded_slant",
-        buffer_padding = 0,
-        show_buffer_numbers = true,
-      },
-      custom_areas = {
-        right = function()
-          local result = {}
-          local seve = vim.diagnostic.severity
-          local error = vim.diagnostic.get(0, { severity = seve.ERROR })
-          local warning = vim.diagnostic.get(0, { severity = seve.WARN })
-          local info = vim.diagnostic.get(0, { severity = seve.INFO })
-          local hint = vim.diagnostic.get(0, { severity = seve.HINT })
+	"akinsho/bufferline.nvim",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				mode                        = "buffers", -- set to "tabs" to only show tabpages instead
+				numbers                     = "ordinal",
+				themable                    = false,
+				modified_icon               = "● ",
+				left_trunc_marker           = " ",
+				right_trunc_marker          = " ",
+				max_name_length             = 30,
+				max_prefix_length           = 20,
+				truncate_names              = true,
+				tab_size                    = 20,
+				diagnostics                 = "nvim_lsp",
+				diagnostics_update_on_event = true,
+				color_icons                 = true,
+				show_buffer_icons           = true,
+				show_buffer_close_icons     = false,
+				show_close_icon             = false,
+				show_tab_indicators         = true,
+				show_duplicate_prefix       = true,
+				duplicates_across_groups    = true,
+				persist_buffer_sort         = true,
+				move_wraps_at_ends          = false,
+				separator_style             = "slant", -- [focused and unfocused]. eg: { '|', '|' }
+				enforce_regular_tabs        = true,
+				always_show_bufferline      = true,
+				auto_toggle_bufferline      = true,
+				indicator                   = { style    = "underline" },
+				pick                        = { alphabet = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ1234567890", },
+				custom_areas = {
+					right = function()
+						local result = {}
+						local seve = vim.diagnostic.severity
+						local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+						local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+						local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+						local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
 
-          if error ~= 0 then
-            table.insert(result, { text = "  " .. error, link = "DiagnosticError" })
-          end
+						if #error ~= 0 then
+							table.insert(result, { text = "  " .. error, link = "DiagnosticError" })
+						end
 
-          if warning ~= 0 then
-            table.insert(result, { text = "  " .. warning, link = "DiagnosticWarn" })
-          end
+						if #warning ~= 0 then
+							table.insert(result, { text = "  " .. warning, link = "DiagnosticWarn" })
+						end
 
-          if hint ~= 0 then
-            table.insert(result, { text = "  " .. hint, link = "DiagnosticHint" })
-          end
+						if #hint ~= 0 then
+							table.insert(result, { text = "  " .. hint, link = "DiagnosticHint" })
+						end
 
-          if info ~= 0 then
-            table.insert(result, { text = "  " .. info, link = "DiagnosticInfo" })
-          end
-          return result
-        end,
-      }
-    })
-  end,
+						if #info ~= 0 then
+							table.insert(result, { text = "  " .. info, link = "DiagnosticInfo" })
+						end
+						return result
+					end,
+				},
+			},
+		})
+	end,
 }

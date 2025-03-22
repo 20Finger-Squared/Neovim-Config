6b5855b04f4f875a94e0cfe122adb882b5675955
Rhylie <orhylie@gmail.com>
2025-03-22 20:49:13 +0000

    fixed keymaps

--- lua/mappings/oil.lua
+++ lua/mappings/oil.lua
@@ -2,31 +2,8 @@ local oil = require("oil")
 local actions = require("oil.actions")
 local set = vim.keymap.set
 
--- ai generated function cause idk how to do stuff aside from basic plugin :)
-local function is_focused_window_oil_float()
-  local win_id = vim.api.nvim_get_current_win()
-  local buf_id = vim.api.nvim_win_get_buf(win_id)
+set("n", "<leader>cd", function() actions.cd.callback() end, { desc = "Change to directory in oil" })
 
-  -- Check if the buffer is an Oil buffer
-  if vim.bo[buf_id].filetype ~= "oil" then
-    return false
-  end
+set("n", "<leader>v", function() actions.select.callback({ vertical = true }) end, { desc = "Open file in vertical split" })
 
-  -- Check if the window is a floating window
-  local win_config = vim.api.nvim_win_get_config(win_id)
-  return win_config.relative ~= ""
-end
-
-set("n", "<leader>cd", function()
-    actions.cd.callback()
-end, { desc = "Change to directory in oil" })
-
-set("n", "<leader>v", function()
-  if is_focused_window_oil_float() then
-    actions.select.callback({ vertical = true })
-  end
-end, { desc = "Open file in vertical split" })
-
-set("n", "-", function()
-  oil.toggle_float()
-end, { desc = "Open oil window" })
+set("n", "-", function() oil.toggle_float() end, { desc = "Open oil window" })

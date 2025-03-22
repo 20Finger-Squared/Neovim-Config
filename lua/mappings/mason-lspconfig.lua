bf1900d38eb12abebaf9f4e777b0bff69ac7e62d
Rhylie <orhylie@gmail.com>
2025-03-22 14:18:07 +0000

    removed if statements

--- lua/mappings/mason-lspconfig.lua
+++ lua/mappings/mason-lspconfig.lua
@@ -1,18 +1,6 @@
 local set = vim.keymap.set
-
-set("n", "gd", function()
-    if vim.lsp.buf.server_ready() then vim.lsp.buf.definition() end
-end, { desc = "Go to definition" })
-
-set("n", "<leader>fm", function()
-    if vim.lsp.buf.server_ready() then vim.lsp.buf.format() end
-end, { desc = "Format code" })
-
-set("n", "K", function()
-    if vim.lsp.buf.server_ready() then vim.lsp.buf.hover() end
-end, { desc = "Show hover documentation" })
-
-set("n", "<leader>ca", function()
-    if vim.lsp.buf.server_ready() then vim.lsp.buf.code_action() end
-end, { desc = "Show code actions" })
+set("n", "<leader>fm", vim.lsp.buf.format(),     { desc = "Format code" })
+set("n", "<leader>ca", vim.lsp.buf.code_action() { desc = "Show code actions" })
+set("n", "gd",         vim.lsp.buf.definition(), { desc = "Go to definition" })
+set("n", "K",          vim.lsp.buf.hover(),      { desc = "Show hover documentation" })
 

4742ad3b6c52afec74189bec379edc5a210e27ab
Rhylie <orhylie@gmail.com>
2025-03-23 13:55:19 +0000

    Fixed spelling

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,7 +8,7 @@ return {
 		require("oil").setup({
       default_file_explorer         = true,
       skip_confirm_for_simple_edits = true,
-      use_defualt_keymaps           = false,
+      use_default_keymaps           = false,
 
 		  view_options = { show_hidden = true },
       columns = {

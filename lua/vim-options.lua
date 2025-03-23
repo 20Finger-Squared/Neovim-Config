d23ea25e65573f841cdd09ac1b042b7487b63d4a
Rhylie <orhylie@gmail.com>
2025-03-23 13:53:33 +0000

    Moved highlighting config
    Moved to plugins/bufferline.lua

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,23 +8,8 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.spelllang = "en_gb"
-vim.g.mapleader = " "
 -- folding settings
 vim.opt.foldenable = true
 vim.opt.foldmethod = "indent"
-vim.opt.foldlevel = 99
-vim.opt.foldcolumn = "1"
-
-vim.api.nvim_set_hl(0, "BufferLineFill", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineBackground", { bg = "NONE" })
-
--- Set the selected buffer's background to transparent as well
-vim.api.nvim_set_hl(0, "BufferLineBufferSelected", { fg = "#ffffff", bg = "NONE", italic = true })
-
--- Set the non-selected buffer's background (can still be transparent)
-vim.api.nvim_set_hl(0, "BufferLineBuffer", { fg = "#ffffff", bg = "NONE", bold = true })
-
--- Set the separator background to transparent
-vim.api.nvim_set_hl(0, "BufferLineSeparator", { fg = "#000000", bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineSeparatorSelected", { fg = "#ffffff", bg = "NONE" })
-
+vim.opt.foldlevel = 3
+vim.opt.foldcolumn = "3"

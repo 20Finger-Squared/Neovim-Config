f179b237df515be3f4afc8a518f393c0392c434f
Rhylie <orhylie@gmail.com>
2025-03-23 13:51:46 +0000

    Added more mapping
    Added more mappings and improved a mapping

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -1,5 +1,7 @@
 local set = vim.keymap.set
 local builtin = require("telescope.builtin")
-set("n", "<C-p>",      builtin.find_files, { desc = "Used to quickly find a file"})
-set("n", "<leader>fg", builtin.live_grep,  { desc = "Used to quickly find code" })
-set("n", "<leader>fo", builtin.oldfiles,   { desc = "Used to find the most recently used files" })
+set("n",    "<C-p>",      builtin.find_files,  { desc = "Used to quickly find a file" })
+set("n",    "<leader>fg", builtin.live_grep,   { desc = "Used to quickly find code" })
+set("n",    "<leader>fo", builtin.oldfiles,    { desc = "Used to find the most recently used files" })
+set("n",    "<leader>di", builtin.diagnostics, { desc = "Lists diagnostics" })
+set( { "n", "i" },        "<C-s>",             builtin.spell_suggest, { desc = "Lists spelling suggestions for the current word under the cursor" })

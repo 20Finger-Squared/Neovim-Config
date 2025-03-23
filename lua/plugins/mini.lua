8de1224ace6c2dbde674c681a985590e07d552ff
Rhylie <orhylie@gmail.com>
2025-03-23 13:48:28 +0000

    Improve configs
    Added more options and removed unused features

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,18 +1,26 @@
 return {
-	{ -- comment
-		"echasnovski/mini.nvim",
-		version = false,
-		config = function()
-			require("mini.misc").setup()
-			require("mini.comment").setup()
-			require("mini.pairs").setup()
-			require("mini.surround").setup()
-			require("mini.align").setup()
-			require("mini.splitjoin").setup()
-      require('mini.clue').setup()
-			require("mini.cursorword").setup()
-			require("mini.ai").setup()
-			require("mini.basics").setup()
-		end,
-	},
+  { -- comment
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+      require("mini.basics").setup({
+        options = {
+          extra_ui = true,
+          win_borders = 'double',
+        },
+        mappings = {
+          windows = true,
+          move_with_alt = true,
+        }
+      })
+
+      require("mini.comment").setup() -- used for the absolute greatness of being able to comment quickly
+      require("mini.pairs").setup()   -- used to quickly add pairs for Characters
+      require("mini.surround").setup() -- used to surround text
+      require("mini.align").setup()   -- better tabular
+      require("mini.splitjoin").setup() -- Good for the J which merges two lines together
+      require("mini.cursorword").setup() -- Used to show identical words
+      require("mini.ai").setup()      -- better text objects such as vit ( visual in tag )
+    end,
+  },
 }

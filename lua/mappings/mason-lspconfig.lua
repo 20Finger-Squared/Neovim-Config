328d950433d5946d445c41d9e07598a2e33264f7
Rhylie <orhylie@gmail.com>
2025-03-23 13:52:37 +0000

    Improved a mapping

--- lua/mappings/mason-lspconfig.lua
+++ lua/mappings/mason-lspconfig.lua
@@ -1,5 +1,5 @@
 local set = vim.keymap.set
 set("n", "<leader>fm", function() vim.lsp.buf.format() end,      { desc = "Format code"              })
 set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "Show code actions"        })
-set("n", "gd",         function() vim.lsp.buf.definition() end,  { desc = "Go to definition"         })
+set("n", "<leader>md", function() vim.lsp.buf.definition() end,  { desc = "Go to definition"         })
 set("n", "K",          function() vim.lsp.buf.hover() end,       { desc = "Show hover documentation" })

81655b39e6bd8fd4276b41fc1460e527a7fdcaa5
Rhylie <orhylie@gmail.com>
2025-03-23 14:05:47 +0000

    Added mapping
    Added mappings to options for a simplier config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,20 +1,38 @@
 return {
-	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
-	dependencies = { "nvim-tree/nvim-web-devicons" }, 
-	lazy = false,
-	config = function()
-		require("oil").setup({
-      default_file_explorer         = true,
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false,
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
       skip_confirm_for_simple_edits = true,
-      use_default_keymaps           = false,
+      use_default_keymaps = false,
 
-		  view_options = { show_hidden = true },
+      view_options = { show_hidden = true },
       columns = {
         "icon",
         "size",
       },
-		})
-	end,
+      keymaps = {
+        ["<CR>"] = "actions.select",
+        ["<C-p>"] = "actions.preview",
+        ["<C-l>"] = "actions.refresh",
+        ["gx"] = "actions.open_external",
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<leader>v"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
+    })
+  end,
 }
+

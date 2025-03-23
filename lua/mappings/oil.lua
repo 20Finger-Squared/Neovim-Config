624f39716147c42b58a55c103a972e9e0b65b424
Rhylie <orhylie@gmail.com>
2025-03-23 14:03:10 +0000

    Simplfied mapping

--- lua/mappings/oil.lua
+++ lua/mappings/oil.lua
@@ -1,9 +1,4 @@
 local oil = require("oil")
-local actions = require("oil.actions")
 local set = vim.keymap.set
-
-set("n", "<leader>cd", function() actions.cd.callback() end, { desc = "Change to directory in oil" })
-
-set("n", "<leader>v", function() actions.select.callback({ vertical = true }) end, { desc = "Open file in vertical split" })
 
 set("n", "-", function() oil.toggle_float() end, { desc = "Open oil window" })

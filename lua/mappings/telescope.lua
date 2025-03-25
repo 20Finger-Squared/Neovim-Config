939845e132ebc7e2ceddb81876569cc46f654462
Rhylie <orhylie@gmail.com>
2025-03-25 07:19:13 +0000

    improved keymapping

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -1,6 +1,6 @@
 local set = vim.keymap.set
 local builtin = require("telescope.builtin")
-set("n",    "<C-p>",      builtin.find_files,  { desc = "Used to quickly find a file" })
+set("n",    "<leader>ff", builtin.find_files,  { desc = "Used to quickly find a file" })
 set("n",    "<leader>fg", builtin.live_grep,   { desc = "Used to quickly find code" })
 set("n",    "<leader>fo", builtin.oldfiles,    { desc = "Used to find the most recently used files" })
 set("n",    "<leader>di", builtin.diagnostics, { desc = "Lists diagnostics" })

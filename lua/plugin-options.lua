272d9f3f4c6017fc782742f973eb3cf6b998878b
Rhylie <orhylie@gmail.com>
2025-03-28 23:44:03 +0000

    Improved organization
    Moved some highlighting into a seperate file

--- lua/plugin-options.lua
+++ lua/plugin-options.lua
@@ -1,4 +1,2 @@
 -- used for options that need to be loaded after plugins not strictly just for plugin options
 vim.cmd("colorscheme gruvbox")
-vim.cmd("highlight Comment guifg=#b0b0b0")
-vim.cmd("highlight Normal guibg=NONE")  -- Make the background transparent

01c082278b5af6b4b61d07fd2413665453c3a203
Rhylie <orhylie@gmail.com>
2025-03-28 23:40:46 +0000

    Better jumping

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,21 +2,31 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {highlight = {
-    backdrop = true,  -- Enable backdrop highlighting
-    groups = {
-      match = "FlashMatch",
-      current = "FlashCurrent",
-      label = "FlashLabel",
-      backdrop = "none",  -- Set backdrop highlight group
+
+  opts = {
+    jump      = { autojump = true },
+    highlight = {},
+    label     = { rainbow = { enabled = true, shade = 5 } },
+    search    = {},
+    ---@type table<string, Flash.Config>
+    modes     = {
+      -- options used when flash is activated through
+      -- a regular search with `/` or `?`
+      search = { enabled = true, highlight = { backdrop = true } },
+      -- options used when flash is activated through
+      -- `f`, `F`, `t`, `T`, `;` and `,` motions
+      char = {
+        enabled = true,
+        jump_labels = true,
+        highlight = { backdrop = false },
+      },
     },
-  }, },
-  -- stylua: ignore
+  },
+
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "fj", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "tS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    { "r",  mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+    { "R",  mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
   },
 }

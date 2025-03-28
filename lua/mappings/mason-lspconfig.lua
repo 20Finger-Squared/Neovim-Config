0961e10ea5f8a7afa5060bfcf128929031d79b9b
Rhylie <orhylie@gmail.com>
2025-03-28 23:39:39 +0000

    Added more mappings

--- lua/mappings/mason-lspconfig.lua
+++ lua/mappings/mason-lspconfig.lua
@@ -1,5 +1,9 @@
 local set = vim.keymap.set
-set("n", "<leader>fm", function() vim.lsp.buf.format() end,      { desc = "Format code"              })
-set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "Show code actions"        })
-set("n", "<leader>md", function() vim.lsp.buf.definition() end,  { desc = "Go to definition"         })
-set("n", "K",          function() vim.lsp.buf.hover() end,       { desc = "Show hover documentation" })
+
+set("n", "<leader>fm", vim.lsp.buf.format,         { desc = "Format code"              })
+set("n", "<leader>ca", vim.lsp.buf.code_action,    { desc = "Show code actions"        })
+set("n", "K",          vim.lsp.buf.hover,          { desc = "Show hover documentation" })
+set("n", "<leader>sh", vim.lsp.buf.signature_help, { desc = "Show signature help" })
+set("n", "gd",         vim.lsp.buf.definition,     { desc = "Go to definition" })
+set("n", "gr",         vim.lsp.buf.references,     { desc = "Find references" })
+set("n", "<leader>rn", vim.lsp.buf.rename,         { desc = "Rename symbol" })
--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -1,7 +1,11 @@
 local set = vim.keymap.set
 local builtin = require("telescope.builtin")
-set("n",    "<leader>ff", builtin.find_files,  { desc = "Used to quickly find a file" })
-set("n",    "<leader>fg", builtin.live_grep,   { desc = "Used to quickly find code" })
-set("n",    "<leader>fo", builtin.oldfiles,    { desc = "Used to find the most recently used files" })
-set("n",    "<leader>di", builtin.diagnostics, { desc = "Lists diagnostics" })
-set( { "n", "i" },        "<C-s>",             builtin.spell_suggest, { desc = "Lists spelling suggestions for the current word under the cursor" })
+set("n", "<leader>ff", builtin.find_files,  { desc = "Used to quickly find a file" })
+set("n", "<leader>km", builtin.keymaps,     { desc = "get all keymasp" })
+set("n", "<leader>fg", builtin.live_grep,   { desc = "Used to find code in a project" })
+set("n", "<leader>fo", builtin.oldfiles,    { desc = "Used to find the most recently used files" })
+set("n", "<leader>di", builtin.diagnostics, { desc = "Lists diagnostics in a project" })
+set("n", "<leader>tm", builtin.marks,       { desc = "Lists vim marks and their value"})
+set("n", "<leader>tr", builtin.registers,   {desc = "Lists vim registers, pastes the contents of the register on enter"})
+
+set({ "n", "i" }, "<C-s>", builtin.spell_suggest, { desc = "Lists spelling suggestions for the current word under the cursor" })

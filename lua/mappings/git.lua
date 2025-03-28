8cccba5dc367e6211ef31dfc4be46ffe2ba42d34
Rhylie <orhylie@gmail.com>
2025-03-28 23:38:53 +0000

    Merged into same file
    Merged and removed unused mappings

--- lua/mappings/git.lua
+++ lua/mappings/git.lua
@@ -1,8 +1,3 @@
-local set = vim.api.nvim_set_keymap
+local set     = vim.keymap.set
 
 set("n", "<leader>lg", ":LazyGit<CR>",              { desc = "LazyGit" })
-set("n", "]c",         ":GitGutterNextHunk<CR>",    { noremap = true, silent = true })
-set("n", "[c",         ":GitGutterPrevHunk<CR>",    { noremap = true, silent = true })
-set("n", "<leader>gs", ":GitGutterStageHunk<CR>",   { noremap = true, silent = true })
-set("n", "<leader>gu", ":GitGutterUndoHunk<CR>",    { noremap = true, silent = true })
-set("n", "<leader>gp", ":GitGutterPreviewHunk<CR>", { noremap = true, silent = true })
--- lua/mappings/gitsigns.lua
+++
@@ -1,6 +0,0 @@
-local set = vim.api.nvim_set_keymap
-set("n", "]c",         ":GitGutterNextHunk<CR>",    { noremap = true, silent = true })
-set("n", "[c",         ":GitGutterPrevHunk<CR>",    { noremap = true, silent = true })
-set("n", "<leader>gs", ":GitGutterStageHunk<CR>",   { noremap = true, silent = true })
-set("n", "<leader>gu", ":GitGutterUndoHunk<CR>",    { noremap = true, silent = true })
-set("n", "<leader>gp", ":GitGutterPreviewHunk<CR>", { noremap = true, silent = true })

176037672058c5d1a67c96e6509ff5d263bc2c3b
Rhylie <orhylie@gmail.com>
2025-03-28 23:40:02 +0000

    Added blink
    Added blink and removed nvim-cmp

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -6,7 +6,12 @@ return {
     -- example using `opts` for defining servers
     opts = {
       servers = {
-        lua_ls = {}
+        lua_ls = {},
+        html = {}, -- Fixed
+        cssls = {},
+        quick_lint_js = {},
+        css_variables = {},
+        ts_ls = {}, -- Fixed
       }
     },
     config = function(_, opts)
@@ -20,7 +25,17 @@ return {
   {
     'saghen/blink.cmp',
     dependencies = {
-      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+      {
+        'l3mon4d3/luasnip',
+        version = 'v2.*',
+        dependencies = {
+          "rafamadriz/friendly-snippets",
+        },
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_lua").lazy_load()
+        end,
+      },
       'neovim/nvim-lspconfig',
       "jdrupal-dev/css-vars.nvim",
       'rafamadriz/friendly-snippets',
@@ -28,35 +43,16 @@ return {
     },
 
 
-    -- use a release tag to download pre-built binaries
     version = '*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
 
     ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
+    ---@type blink.cmp.config
     opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = {
         preset      = 'default',
-        ['<Tab>']   = { 'select_next', 'fallback' },
-        ['<S-Tab>'] = { 'select_prev', 'fallback' },
-        ['<CR>']    = { 'accept', 'fallback' },
-
-
+        ['<tab>']   = { 'select_next', 'fallback' },
+        ['<s-tab>'] = { 'select_prev', 'fallback' },
+        ['<cr>']    = { 'accept', 'fallback' },
       },
 
       appearance = {
@@ -72,30 +68,31 @@ return {
           }
         }
       },
-      -- (Default) Only show the documentation popup when manually triggered
+      -- (default) only show the documentation popup when manually triggered
       completion = {
         documentation = {
           auto_show = false,
           auto_show_delay_ms = 500,
         },
 
+
         accept = { auto_brackets = { enabled = true } },
         ghost_text = { enabled = true },
         menu = {
           draw = {
-            -- Aligns the keyword you've typed to a component in the menu
+            -- aligns the keyword you've typed to a component in the menu
             align_to = 'label', -- or 'none' to disable, or 'cursor' to align to the cursor
-            -- Left and right padding, optionally { left, right } for different padding on each side
+            -- left and right padding, optionally { left, right } for different padding on each side
             padding = 1,
-            -- Gap between columns
+            -- gap between columns
             gap = 1,
-            -- Use treesitter to highlight the label text for the given list of sources
+            -- use treesitter to highlight the label text for the given list of sources
             treesitter = { 'lsp' },
 
-            -- Components to render, grouped by column
+            -- components to render, grouped by column
             columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
 
-            -- Definitions for possible components to render. Each defines:
+            -- definitions for possible components to render. each defines:
             --   ellipsis: whether to add an ellipsis when truncating the text
             --   width: control the min, max and fill behavior of the component
             --   text function: will be called for each item
@@ -120,16 +117,16 @@ return {
                 highlight = function(ctx)
                   -- label and label details
                   local highlights = {
-                    { 0, #ctx.label, group = ctx.deprecated and 'BlinkCmpLabelDeprecated' or 'BlinkCmpLabel' },
+                    { 0, #ctx.label, group = ctx.deprecated and 'blinkcmplabeldeprecated' or 'blinkcmplabel' },
                   }
                   if ctx.label_detail then
                     table.insert(highlights,
-                      { #ctx.label, #ctx.label + #ctx.label_detail, group = 'BlinkCmpLabelDetail' })
+                      { #ctx.label, #ctx.label + #ctx.label_detail, group = 'blinkcmplabeldetail' })
                   end
 
                   -- characters matched on the label by the fuzzy matcher
                   for _, idx in ipairs(ctx.label_matched_indices) do
-                    table.insert(highlights, { idx, idx + 1, group = 'BlinkCmpLabelMatch' })
+                    table.insert(highlights, { idx, idx + 1, group = 'blinkcmplabelmatch' })
                   end
 
                   return highlights
@@ -139,40 +136,40 @@ return {
               label_description = {
                 width = { max = 30 },
                 text = function(ctx) return ctx.label_description end,
-                highlight = 'BlinkCmpLabelDescription',
+                highlight = 'blinkcmplabeldescription',
               },
 
               source_name = {
                 width = { max = 30 },
                 text = function(ctx) return ctx.source_name end,
-                highlight = 'BlinkCmpSource',
+                highlight = 'blinkcmpsource',
               },
 
               source_id = {
                 width = { max = 30 },
                 text = function(ctx) return ctx.source_id end,
-                highlight = 'BlinkCmpSource',
+                highlight = 'blinkcmpsource',
               },
             },
           },
           enabled = true,
           min_width = 15,
           max_height = 10,
-          border = "rounded", -- Defaults to `vim.o.winborder` on nvim 0.11+
+          border = nil, -- defaults to `vim.o.winborder` on nvim 0.11+
           winblend = 0,
           winhighlight =
-          'Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None',
-          -- Keep the cursor X lines away from the top/bottom of the window
-          scrolloff = 2,
-          -- Note that the gutter will be disabled when border ~= 'none'
+          'normal:blinkcmpmenu,floatborder:blinkcmpmenuborder,cursorline:blinkcmpmenuselection,search:none',
+          -- keep the cursor x lines away from the top/bottom of the window
+          scrolloff = 3,
+          -- note that the gutter will be disabled when border ~= 'none'
           scrollbar = false,
-          -- Which directions to show the window,
+          -- which directions to show the window,
           -- falling back to the next direction when there's not enough space
-          direction_priority = { 's', 'n' },
+          direction_priority = { 'n', 's' },
 
           auto_show = true,
 
-          -- Screen coordinates of the command line
+          -- screen coordinates of the command line
           cmdline_position = function()
             if vim.g.ui_cmdline_pos ~= nil then
               local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
@@ -184,7 +181,7 @@ return {
         }
       },
 
-      -- Default list of enabled providers defined so that you can extend it
+      -- default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
         default = {
@@ -196,29 +193,32 @@ return {
           "emoji",
         },
         providers = {
-
           css_vars = {
             name = "css-vars",
             module = "css-vars.blink",
-            opts = {
-              -- WARNING: The search is not optimized to look for variables in JS files.
-              -- If you change the search_extensions you might get false positives and weird completion results.
-              search_extensions = { ".js", ".ts", ".jsx", ".tsx" }
-            }
+            opts = { search_extensions = { ".js", ".ts", ".jsx", ".tsx" } }
           },
 
           emoji = {
             module = "blink-emoji",
-            name = "Emoji",
-            score_offset = 15,        -- Tune by preference
-            opts = { insert = true }, -- Insert emoji (default) or complete its name
+            name = "emoji",
+            score_offset = 15,        -- tune by preference
+            opts = { insert = true }, -- insert emoji (default) or complete its name
           },
+
+          lsp = {
+            name = 'lsp',
+            module = "blink.cmp.sources.lsp",
+            enabled = true,
+            should_show_items = true,
+          }
+
         },
       },
 
-      -- Experimental signature help support
+      -- experimental signature help support
       signature = {
-        enabled = false,
+        enabled = true,
         trigger = {
           enabled = true,
           show_on_keyword = false,
@@ -232,26 +232,22 @@ return {
           min_width = 1,
           max_width = 100,
           max_height = 10,
-          border = "rounded", -- Defaults to `vim.o.winborder` on nvim 0.11+ or 'padded' when not defined/<=0.10
+          border = nil, -- defaults to `vim.o.winborder` on nvim 0.11+ or 'padded' when not defined/<=0.10b
           winblend = 0,
-          winhighlight = 'Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder',
-          scrollbar = false, -- Note that the gutter will be disabled when border ~= 'none'
-          -- Which directions to show the window,
+          winhighlight = 'normal:blinkcmpsignaturehelp,floatborder:blinkcmpsignaturehelpborder',
+          scrollbar = false, -- note that the gutter will be disabled when border ~= 'none'
+          -- which directions to show the window,
           -- falling back to the next direction when there's not enough space,
           -- or another window is in the way
           direction_priority = { 'n', 's' },
-          -- Disable if you run into performance issues
+          -- disable if you run into performance issues
           treesitter_highlighting = true,
           show_documentation = true,
         },
       },
+
       snippets = { preset = 'luasnip' },
 
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
       fuzzy = { implementation = "prefer_rust_with_warning" }
     },
     opts_extend = { "sources.default" }

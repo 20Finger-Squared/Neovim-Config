be59989e937f1857a80fec81d052f3c923247613
Rhylie <orhylie@gmail.com>
2025-03-28 23:39:30 +0000

    formatting

--- lua/mappings/harpoon.lua
+++ lua/mappings/harpoon.lua
@@ -6,6 +6,7 @@ harpoon:setup()
 set("n", "<leader>a", function() harpoon:list():add() end,                         {desc = "Adds file to harpoon"})
 set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = "Open harpoon select menu"})
 
+
 set("n", "<leader>1", function() harpoon:list():select(1) end, {desc="Opens file no.1 in the harpoon buffer"})
 set("n", "<leader>2", function() harpoon:list():select(2) end, {desc="Opens file no.2 in the harpoon buffer"})
 set("n", "<leader>3", function() harpoon:list():select(3) end, {desc="Opens file no.3 in the harpoon buffer"})
--- lua/mappings/vim-mappings.lua
+++ lua/mappings/vim-mappings.lua
@@ -1,3 +1,3 @@
 local set = vim.keymap.set
-set("n", ";",          ":",                              { desc = "Makes it more easy to enter cmdline" })
-set("n", "<leader>wb", ":bdelete<CR>",                   { desc = "Close current buffer" })
+set("n", ";", ":", { desc = "Makes it more easy to enter cmdline" })
+set("n", "<leader>wb", ":bdelete<CR>", { desc = "Close current buffer" })

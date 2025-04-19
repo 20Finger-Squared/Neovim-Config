8fe7ab2fe4760df558f566aa310c266f7a837b90
20Finger-Squared <orhylie@gmail.com>
2025-04-19 15:06:17 +0100

    Removed because redundant

--- lua/mappings/bufferline.lua
+++
@@ -1,5 +0,0 @@
-local set = vim.keymap.set
-set("n", "<Tab>",           ":BufferLineCycleNext<CR>",  {desc = "Next buffer"})
-set("n", "<S-Tab>",         ":BufferLineCyclePrev<CR>",  {desc = "Previous buffer"})
-set("n", "<Leader><Tab>",   ":BufferLineCloseRight<CR>", {desc = "Close all buffers to the right"})
-set("n", "<Leader><S-Tab>", ":BufferLineCloseLeft<CR>",  {desc = "Close all buffers to the left"})

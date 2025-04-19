8811fe6fd3389dd0cab280eef65a7ddacf6cce9d
20Finger-Squared <orhylie@gmail.com>
2025-04-19 15:04:53 +0100

    Improved UI
    Made UI vertical to make reading easier

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,14 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
   dependancies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        layout_strategy = 'vertical',
+        layout_config = {
+          width =  0.7,
+        },
+      }
+    })
+  end
 }

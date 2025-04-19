24295c921542afb59d3da1c186e6621564eeb360
20Finger-Squared <orhylie@gmail.com>
2025-04-19 15:03:09 +0100

    Removed background transparency

--- lua/vim-highlighting.lua
+++ lua/vim-highlighting.lua
@@ -3,5 +3,4 @@ hl(0, "DiagnosticSignWarn",  { bg = "NONE" })   -- Orange foreground for warning
 hl(2, "DiagnosticSignInfo",  { bg = "NONE" })    -- Blue foreground for info
 hl(0, "DiagnosticSignHint",  { bg = "NONE" })    -- Green foreground for hints
 hl(0, "DiagnosticSignError", { bg = "NONE" })    -- Green foreground for hints
-hl(0, "Normal",              { bg = "NONE" })
 hl(0, "Comment",             { fg="#b0b0b0" })

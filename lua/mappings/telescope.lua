a594274aca7eeb66072ad1efbe738d4f635bb8be
20Finger-Squared <orhylie@gmail.com>
2025-04-29 19:36:59 +0100

    Added symbols plus new bindings

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -1,11 +1,18 @@
 local set = vim.keymap.set
 local builtin = require("telescope.builtin")
-set("n", "<leader>ff", builtin.find_files,  { desc = "Used to quickly find a file" })
-set("n", "<leader>km", builtin.keymaps,     { desc = "get all keymasp" })
-set("n", "<leader>fg", builtin.live_grep,   { desc = "Used to find code in a project" })
-set("n", "<leader>fo", builtin.oldfiles,    { desc = "Used to find the most recently used files" })
-set("n", "<leader>di", builtin.diagnostics, { desc = "Lists diagnostics in a project" })
-set("n", "<leader>tm", builtin.marks,       { desc = "Lists vim marks and their value"})
-set("n", "<leader>tr", builtin.registers,   {desc = "Lists vim registers, pastes the contents of the register on enter"})
 
-set({ "n", "i" }, "<C-s>", builtin.spell_suggest, { desc = "Lists spelling suggestions for the current word under the cursor" })
+set("n", "<leader>ff", builtin.find_files,  { desc = "󰈞 Find files"   })
+set("n", "<leader>fg", builtin.live_grep,   { desc = "󰱼 Grep project" })
+set("n", "<leader>fo", builtin.oldfiles,    { desc = "󰄉 Recent files" })
+
+set("n", "<leader>tdi", builtin.diagnostics, { desc = " Diagnostics"     })
+set("n", "<leader>th",  builtin.colorscheme, { desc = "󰌗 Pick colorscheme"})
+set("n", "<leader>tm",  builtin.marks,       { desc = " Vim marks"       })
+set("n", "<leader>tr",  builtin.registers,   { desc = " Registers"       })
+set("n", "<leader>tkm", builtin.keymaps,     { desc = " Show keymaps"    })
+set("n", "<leader>tfc", builtin.commands,    { desc = "󰘳 Commands"        })
+set("n", "<leader>tfh", builtin.help_tags,   { desc = "󰍜 Help tags"       })
+set("n", "<leader>tfq", builtin.quickfix,    { desc = " Quickfix list"   })
+set("n", "<leader>tfj", builtin.jumplist,    { desc = " Jumplist"        })
+
+set({ "n", "i" }, "<C-s>", builtin.spell_suggest, { desc = "󰓆 Spelling suggestions" }) -- spellcheck

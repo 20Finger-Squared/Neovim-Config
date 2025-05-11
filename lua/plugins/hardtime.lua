dd7a220508b41ed9dfa5fcc48fc598865f16aebe
20Finger-Squared <orhylie@gmail.com>
2025-05-12 00:30:19 +0100

    Removed because uneeded

--- lua/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "m4xshen/hardtime.nvim",
-  dependencies = {},
-  opts = {
-    enabled = true,
-    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
-    disable_mouse = false, -- I know right?
-  }
-}

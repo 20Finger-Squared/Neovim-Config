08f919899ba131750a14d43786fd54d7548d75c4
20Finger-Squared <orhylie@gmail.com>
2025-05-12 00:32:00 +0100

    Added a new module

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,20 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  dependancies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("telescope").setup({
-      defaults = {
-        layout_strategy = 'vertical',
-        layout_config = {
-          width =  0.7,
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+
+            "nvim-telescope/telescope-frecency.nvim",
         },
-      }
-    })
-  end
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    layout_strategy = 'vertical',
+                    layout_config = {
+                        width =  0.7,
+                    },
+                },
+            })
+            require("telescope").load_extension( "frecency" )
+        end
 }

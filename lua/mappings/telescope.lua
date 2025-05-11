ab73133b53bbb72b4e0b09d043dac2c6fb61bbf6
20Finger-Squared <orhylie@gmail.com>
2025-05-12 00:21:53 +0100

    Better keymaps

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -1,18 +1,24 @@
-local set = vim.keymap.set
-local builtin = require("telescope.builtin")
+local set       = vim.keymap.set
+local builtin   = require("telescope.builtin")
+local extension = require("telescope").extensions
 
+-- built-in pickers
 set("n", "<leader>ff", builtin.find_files,  { desc = "󰈞 Find files"   })
 set("n", "<leader>fg", builtin.live_grep,   { desc = "󰱼 Grep project" })
 set("n", "<leader>fo", builtin.oldfiles,    { desc = "󰄉 Recent files" })
 
-set("n", "<leader>tdi", builtin.diagnostics, { desc = " Diagnostics"     })
-set("n", "<leader>th",  builtin.colorscheme, { desc = "󰌗 Pick colorscheme"})
-set("n", "<leader>tm",  builtin.marks,       { desc = " Vim marks"       })
-set("n", "<leader>tr",  builtin.registers,   { desc = " Registers"       })
-set("n", "<leader>tkm", builtin.keymaps,     { desc = " Show keymaps"    })
-set("n", "<leader>tfc", builtin.commands,    { desc = "󰘳 Commands"        })
-set("n", "<leader>tfh", builtin.help_tags,   { desc = "󰍜 Help tags"       })
-set("n", "<leader>tfq", builtin.quickfix,    { desc = " Quickfix list"   })
-set("n", "<leader>tfj", builtin.jumplist,    { desc = " Jumplist"        })
+set("n", "<leader>th", builtin.colorscheme,         { desc = "󰌗 Pick colorscheme"})
+set("n", "<leader>tm", builtin.marks,               { desc = " Vim marks"       })
+set("n", "<leader>tr", builtin.registers,           { desc = " Registers"       })
+set("n", "<leader>tb", builtin.buffers,             { desc = " Buffers"         })
+set("n", "<leader>tf", extension.frecency.frecency, { desc = "󰉋 Frecency"        })
 
-set({ "n", "i" }, "<C-s>", builtin.spell_suggest, { desc = "󰓆 Spelling suggestions" }) -- spellcheck
+set("n", "<leader>tdi", builtin.diagnostics, { desc = " Diagnostics"     })
+set("n", "<leader>tkm", builtin.keymaps,     { desc = " Show keymaps"    })
+set("n", "<leader>tcc", builtin.commands,    { desc = "󰘳 Commands"        })
+set("n", "<leader>tch", builtin.help_tags,   { desc = "󰍜 Help tags"       })
+set("n", "<leader>tcq", builtin.quickfix,    { desc = " Quickfix list"   })
+set("n", "<leader>tcj", builtin.jumplist,    { desc = " Jumplist"        })
+
+-- spell suggestion in normal + insert
+set({ "n", "i" }, "<C-s>", builtin.spell_suggest, { desc = "󰓆 Spelling suggestions" })
--- lua/mappings/vim-mappings.lua
+++ lua/mappings/vim-mappings.lua
@@ -1,3 +1,15 @@
 local set = vim.keymap.set
 set("n", ";", ":", { desc = "Makes it more easy to enter cmdline" })
 set("n", "<leader>wb", ":bdelete<CR>", { desc = "Close current buffer" })
+
+vim.keymap.set("n", "<leader>rp", function()
+        if vim.bo.filetype == "python" then
+            vim.cmd("belowright split | terminal python3 " .. vim.fn.expand("%"))
+        end
+        if vim.bo.filetype == "c" then
+            vim.cmd("belowright split | terminal gcc " .. vim.fn.expand("%") .. " -o temp && ./temp")
+        end
+        if vim.bo.filetype == "cpp" then
+            vim.cmd("belowright split | terminal g++ " .. vim.fn.expand("%") .. " -o temp && ./temp")
+        end
+end, { desc = "Runs the current file works with c, cpp, python", silent = true })

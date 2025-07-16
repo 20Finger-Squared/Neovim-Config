f07d1a2acfedbfb7380b6e3c487a026186982ae9
20Finger-Squared <orhylie@gmail.com>
2025-07-16 20:39:50 +0100

    Better defaults

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,33 +1,21 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      ---@type Flash.Config
+      opts = {
+        mode = "fuzzy",
+        jump = {
+            autojump = true,
+            raindow = { enabled = true },
+        }
 
-  opts = {
-    jump      = { autojump = true },
-    highlight = {},
-    label     = { rainbow = { enabled = true, shade = 4 } },
-    search    = {},
-
-    ---@type table<string, Flash.Config>
-    modes     = {
-      -- options used when flash is activated through
-      -- a regular search with `/` or `?`
-      search = { enabled = true, highlight = { backdrop = true } },
-      -- options used when flash is activated through
-      -- `f`, `F`, `t`, `T`, `;` and `,` motions
-      char = {
-        enabled = true,
-        jump_labels = true,
-        highlight = { backdrop = false },
-      },
     },
-  },
-
-  keys = {
-    { "fj", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-    { "tS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    { "r",  mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-    { "R",  mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-  },
+      -- stylua: ignore
+      keys = {
+        { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<leader>I", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      },
 }

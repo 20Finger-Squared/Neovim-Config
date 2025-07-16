ef280734e6ed899048390e989dc6441a49da6d9f
20Finger-Squared <orhylie@gmail.com>
2025-07-16 20:39:08 +0100

    Rust again

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'simrat39/rust-tools.nvim'
+    },
+    {
+        'mrcjkb/rustaceanvim',
+        version = '^6', -- Recommended
+        lazy = false, -- This plugin is already lazy
+    }
+}

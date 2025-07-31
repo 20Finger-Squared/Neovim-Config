d2f3328b02e1d0afe89ce56f7b89518dcda1bad0
20Finger-Squared <orhylie@gmail.com>
2025-07-31 16:40:01 +0100

    new package manager

---
+++ lua/plugins.lua
@@ -0,0 +1,10 @@
+vim.pack.add({
+	{ -- multi tool
+		src="git@github.com:echasnovski/mini.nvim.git",
+		name="mini",
+	},
+    { -- color scheme
+        src="git@github.com:ellisonleao/gruvbox.nvim.git",
+        name="gruvbox",
+    },
+})

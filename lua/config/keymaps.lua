b375f46a39c2640a4ef331cb504ffcf518556b0a
20Finger-Squared <orhylie@gmail.com>
2025-08-29 10:23:13 +0100

    New telescope binds for registers and marks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,7 +15,10 @@ map("n", "<leader>ff", builtin.find_files, { desc = "󰈔 Find files" })
 map("n", "<leader>fg", builtin.live_grep, { desc = "󰱼 Live grep" })
 map("n", "<leader>fh", builtin.help_tags, { desc = "󰋖 Help tags" })
 map("n", "<leader>fm", builtin.keymaps, { desc = "󰌌 Find keymaps" })
+map("n", "<leader>f\"", builtin.registers, { desc = "󰌌 Find registers" })
+map("n", "<leader>f'", builtin.marks, { desc = "󰋖 Find marks" })
 map("n", "<leader>fo", builtin.oldfiles, { desc = "󰋚 Recent files" })
+
 map("n", "<C-s>", builtin.spell_suggest, { desc = "󰓆 Spell suggestions" })
 
 -- Disable arrow keys

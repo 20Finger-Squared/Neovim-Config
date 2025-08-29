6fa407f171017576c4f2eb3fa59f87d1ec5edf04
20Finger-Squared <orhylie@gmail.com>
2025-08-29 10:13:02 +0100

    Noice changed presets
    Changed presets and added some markdown config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,11 +1,22 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+            },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = true,        -- add a border to hover docs and signature help
+        },
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
     }
 }
